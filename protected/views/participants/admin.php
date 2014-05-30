516f18db5b3a4918165689b23508e34168ecff6a
root <macristina@yahoo.com>
2014-05-30 18:01:20 +0800

    convert dropdown to select2,add datepicker

--- protected/views/participants/admin.php
+++ protected/views/participants/admin.php
@@ -1,22 +1,36 @@
 <h1>Participants</h1>
-
+<style>
+  .select2-container{
+    width: 235px;
+  }
+  #birthdate{
+    color: #fff;
+  }
+</style>
 <?php $this->widget('bootstrap.widgets.TbGridView',array(
-'id'=>'participants-grid',
-'dataProvider'=>$model->search(),
-'filter'=>$model,
-'columns'=>array(
-		array('name'=>'group_id','filter'=>CHtml::listData(Groups::model()->findAll(),'id','name'),'value'=>'$data->group->name'),
-		'first_name',
-		'middle_name',
-		'last_name',
-		'birthdate',
-		'contact_number',
-		array('name'=>'include','filter'=>array('1'=>'Yes','0'=>'No'),'value'=>'$data->include == 1 ? "Yes" : "No"'),
-array(
-'class'=>'bootstrap.widgets.TbButtonColumn',
-'template'=>'{update}',
-),
-),
+  'id'=>'participants-grid',
+  'dataProvider'=>$model->search(),
+  'afterAjaxUpdate'=>"function() {
+    jQuery('#group_id').select2({'placeholder':' ','allowClear':true});
+    jQuery('#birthdate').datepicker({'format':'yyyy-mm-dd','autoclose':true}).css('color','#555');
+  }",
+  'filter'=>$model,
+  'columns'=>array(
+     array('name'=>'group_id','filter' => $this->widget('bootstrap.widgets.TbSelect2',array(
+       'model'=>$model,'attribute'=>'group_id','data'=>CHtml::listData(Groups::model()->findAll(),'id','name'),'options' => array(
+         'placeholder' => ' ','allowClear'=>true),'htmlOptions'=>array(
+           'style'=>'width:190px','id'=>'group_id')),true),'value'=>'$data->group->name'),
+     'first_name',
+     'middle_name',
+     'last_name',
+     array('name'=>'birthdate','filter'=>$this->widget('bootstrap.widgets.TbDatePicker',array(
+       'model'=>$model,'attribute'=>'birthdate','options'=>array(
+         'format'=>'yyyy-mm-dd','autoclose'=>true),'htmlOptions'=>array(
+           'id'=>'birthdate','class'=>'input-append date')),true),'sortable'=>true,),
+     'contact_number',
+     array('name'=>'include','filter'=>array('1'=>'Yes','0'=>'No'),'value'=>'$data->include == 1 ? "Yes" : "No"'),
+     array('class'=>'bootstrap.widgets.TbButtonColumn','template'=>'{update}',),
+   ),
 )); ?>
 
 <div class= "clear-fix">
--- protected/views/winners/admin.php
+++ protected/views/winners/admin.php
@@ -1,25 +1,42 @@
 <h1>Winners</h1>
+<style>
+  .select2-container{
+    width: 235px;
+  }
+</style>
 <?php $this->widget('bootstrap.widgets.TbGridView',array(
-'id'=>'winners-grid',
-'dataProvider'=>$model->search(),
-'afterAjaxUpdate'=>"function() {
-  jQuery('#datetime').datetimepicker({'format':'yyyy-mm-dd hh:ii','autoclose':true,'showMeridian':true,'minuteStep':1});
-}",
-'filter'=>$model,
-'columns'=>array(
-		array('name'=>'raffle_id','filter'=>CHtml::listData(Raffles::model()->findAll(),'id','name'),'value'=>'$data->raffle->name'),
-	//	array('name'=>'raffle_id','filter'=>$this->widget('bootstrap.widgets.TbSelect2'),
-	//		array('model'=>$model,'attribute'=>'raffle_id','data'=>CHtml::listData(Raffles::model()->findAll(),'id','name'))),
-		array('name'=>'event_id','filter'=>CHtml::listData(Events::model()->findAll(),'id','name'),'value'=>'$data->event->name'),
-		array('name'=>'participant_id','filter'=>CHtml::listData(Participants::model()->findAll(array('order'=>'last_name')),'id','concatened'),
-			'value'=>'Participants::model()->getName($data->participant_id)'),
-		array('name'=>'group_id','filter'=>CHtml::listData(Groups::model()->findAll(),'id','name'),'value'=>'$data->group->name'),
-		array('name'=>'datetime','filter'=>$this->widget('bootstrap.widgets.TbDateTimePicker',
-			array('model'=>$model,'attribute'=>'datetime','options'=>array('format'=>'yyyy-mm-dd hh:ii','autoclose'=>true,'showMeridian'=>true,'minuteStep'=>1),'htmlOptions'=>array('id'=>'datetime','class'=>'input-append date')),true),'sortable'=>true,
-		),
-array(
-'class'=>'bootstrap.widgets.TbButtonColumn',
-'template'=>'{delete}',
-),
-),
+  'id'=>'winners-grid',
+  'dataProvider'=>$model->search(),
+  'afterAjaxUpdate'=>"function() {
+    jQuery('#raffle_id').select2({'placeholder':' ','allowClear':true});
+    jQuery('#event_id').select2({'placeholder':' ','allowClear':true});
+    jQuery('#participant_id').select2({'placeholder':' ','allowClear':true});
+    jQuery('#group_id').select2({'placeholder':' ','allowClear':true});
+    jQuery('#datetime').datetimepicker({'format':'yyyy-mm-dd hh:ii','autoclose':true,'showMeridian':true,'minuteStep':1});
+  }",
+  'filter'=>$model,
+  'columns'=>array(
+    array('name'=>'raffle_id','filter' => $this->widget('bootstrap.widgets.TbSelect2',array(
+     'model'=>$model,'attribute'=>'raffle_id','data'=>CHtml::listData(Raffles::model()->findAll(),'id','name'),'options' => array(
+       'placeholder' =>' ','allowClear'=>true),'htmlOptions'=>array(
+         'style'=>'width:195px','id'=>'raffle_id')),true),'value'=>'$data->raffle->name',),
+    array('name'=>'event_id','filter' => $this->widget('bootstrap.widgets.TbSelect2',array(
+      'model'=>$model,'attribute'=>'event_id','data'=>CHtml::listData(Events::model()->findAll(),'id','name'),'options' => array(
+        'placeholder' => ' ','allowClear'=>true),'htmlOptions'=>array(
+          'style'=>'width:195px','id'=>'event_id')),true),'value'=>'$data->event->name',),
+    array('name'=>'participant_id','filter' => $this->widget('bootstrap.widgets.TbSelect2',array(
+      'model'=>$model,'attribute'=>'participant_id',
+        'data'=>CHtml::listData(Participants::model()->findAll(array('order'=>'last_name')),'id','concatened'),'options' => array(
+          'placeholder' => ' ','allowClear'=>true),'htmlOptions'=>array(
+            'style'=>'width:235px','id'=>'participant_id')),true),'value'=>'Participants::model()->getName($data->participant_id)'),	
+     array('name'=>'group_id','filter' => $this->widget('bootstrap.widgets.TbSelect2',array(
+       'model'=>$model,'attribute'=>'group_id','data'=>CHtml::listData(Groups::model()->findAll(),'id','name'),'options' => array(
+         'placeholder' => ' ','allowClear'=>true),'htmlOptions'=>array(
+           'style'=>'width:195px','id'=>'group_id')),true),'value'=>'$data->group->name',),
+     array('name'=>'datetime','filter'=>$this->widget('bootstrap.widgets.TbDateTimePicker',array(
+       'model'=>$model,'attribute'=>'datetime','options'=>array(
+         'format'=>'yyyy-mm-dd hh:ii','autoclose'=>true,'showMeridian'=>true,'minuteStep'=>1),'htmlOptions'=>array(
+           'id'=>'datetime','class'=>'input-append date')),true),'sortable'=>true,),
+     array('class'=>'bootstrap.widgets.TbButtonColumn','template'=>'{delete}',),
+  ),
 )); ?>
