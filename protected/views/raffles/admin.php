bff1131ffe5a9f73c0fbe53462b002a6f6453bfe
macristina <macristina@yahoo.com>
2014-05-30 18:33:23 +0800

    convert dropdown to select2 in Raffle

--- protected/views/raffles/admin.php
+++ protected/views/raffles/admin.php
@@ -1,11 +1,24 @@
 <h1>Raffles</h1>
+<style>
+  .select2-container{
+    width: 235px;
+  }
+</style>
 
 <?php $this->widget('bootstrap.widgets.TbGridView',array(
 'id'=>'raffles-grid',
 'dataProvider'=>$model->search(),
+ 'afterAjaxUpdate'=>"function() {
+    jQuery('#event_id').select2({'placeholder':' ','allowClear':true});
+  }",
 'filter'=>$model,
 'columns'=>array(
-		array('name'=>'event_id','filter'=>CHtml::listData(Events::model()->findAll(),'id','name'),'value'=>'$data->event->name'),
+
+     array('name'=>'event_id','filter' => $this->widget('bootstrap.widgets.TbSelect2',array(
+       'model'=>$model,'attribute'=>'event_id','data'=>CHtml::listData(Events::model()->findAll(),'id','name'),'options' => array(
+         'placeholder' => ' ','allowClear'=>true),'htmlOptions'=>array(
+           'style'=>'width:190px','id'=>'event_id')),true),'value'=>'$data->event->name'),
+	//	array('name'=>'event_id','filter'=>CHtml::listData(Events::model()->findAll(),'id','name'),'value'=>'$data->event->name'),
 		'name',
 		'description',
 		'prize',
